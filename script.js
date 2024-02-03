2da6024dcd9fb7cac1f20b8eb7ecda814c123e79
Yik Yin Cheuk <ycheuk@c122h162.wless.reed.edu>
2024-02-02 17:42:40 -0800

    added page

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var header = document.querySelector(".header");
+    var header = document.querySelector("header");
 
     window.addEventListener("scroll", function () {
         var scrollPosition = window.scrollY;
 
         if (scrollPosition > 50) {
-            header.style.backgroundColor = "#333"; /* Change the background color when scrolled */
+            header.style.backgroundColor = "#00373d"; /* Change the background color when scrolled */
         } else {
-            header.style.backgroundColor = "rgba(0, 0, 0, 0.5)"; /* Set the initial transparent background */
+            header.style.backgroundColor = "rgba(0, 55, 61, 0.2)"; /* Set the initial transparent background */
         }
     });
 });
