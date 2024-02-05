44c41b24a537f8baf25923fb6e1c98baaba0582b
Yik Yin Cheuk <ycheuk@c122h162.wless.reed.edu>
2024-02-04 18:27:12 -0800

    added menu

---
+++ menu.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var header = document.querySelector("header");
+
+    window.addEventListener("scroll", function () {
+        var scrollPosition = window.scrollY;
+
+        if (scrollPosition > 50) {
+            header.style.backgroundColor = "#631916"; /* Change the background color when scrolled */
+        } else {
+            header.style.backgroundColor = "rgba(0, 55, 61, 0.2)"; /* Set the initial transparent background */
+        }
+    });
+});
